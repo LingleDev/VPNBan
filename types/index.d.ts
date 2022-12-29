61074c7fde540b5ff396ef16a675c1e51a6b1e66
Gideon Lingle <gideon.lingle2023@gmail.com>
2022-12-29 05:35:30 +0100

    Fix typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,10 @@
+import { Request, Response, NextFunction } from "express"
+
 export = VPNBan;
 declare class VPNBan {
     /**
    * Initializes VPNBan
-   * @param {Object} options
+   * @param {Object} options VPNBan options
    * @param {Boolean} options.vpn A boolean that, if false, will disable VPN blocking; defaults to true
    * @param {Array} options.blocked_ips An array of IP addresses that you want to block; defaults to empty
    * @param {Function} options.callback A callback function that is called if an IP is flagged and blocked.
@@ -10,11 +12,11 @@ declare class VPNBan {
    */
     constructor(options: {
         vpn: boolean;
-        blocked_ips: any[];
+        blocked_ips: string[];
         callback: Function;
     });
     vpn: true;
     blocked_ips: any[];
     callback: Function;
-    block(req: any, res: any, next: any): Promise<any>;
+    block(req: Request, res: Response, next: NextFunction): Promise<any>;
 }
